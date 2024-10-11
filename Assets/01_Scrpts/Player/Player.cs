c2837dffff2a3aea5a09b25f14e0c20a9e0c0156
Mauricio Alejandro Escobar Sejas <mauricionline123@gmail.com>
2024-10-11 15:23:47 -0400

    Adding player script

--- Assets/01_Scrpts/Player/Player.cs
+++ Assets/01_Scrpts/Player/Player.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public int maxHealth = 100;
+
+    private int currentHealth;
+    private bool isDead = false;
+    private Rigidbody rb;
+    private Vector3 movement;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isDead) return;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+        movement.y = 0;
+    }
+
+    void FixedUpdate()
+    {
+        if (isDead) return;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead) return;
+
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        if (isDead) return;
+
+        currentHealth += healAmount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        Debug.Log("Salud del jugador: " + currentHealth);
+    }
+
+    void Die()
+    {
+        isDead = true;
+        Debug.Log("El jugador ha muerto.");
+        // Aquí añadir lógica para mostrar una pantalla de "Game Over"
+        gameObject.SetActive(false);
     }
 }
